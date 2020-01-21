5d9f03fa71a624c226968a80905c37dc85c9218b
Obed Junias <obedjunias.cs17@bmsce.ac.in>
2020-01-21 21:26:16 +0530

    Updates

---
+++ precache-manifest.f935fe51f8ec46ae76483d6948a2fad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5875fef0b71192d3ba4a369b23cc8b53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed435fcca985dd162cc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "753d5a74b02791646627",
+    "url": "/static/js/2.9025fa86.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9025fa86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aed435fcca985dd162cc",
+    "url": "/static/js/main.0a6e1c6e.chunk.js"
+  },
+  {
+    "revision": "7487a6ec5b191e6bee19",
+    "url": "/static/js/runtime-main.bb8ebf9e.js"
+  }
+]);
